2af4cefe4babcc4ae24dc64c8620f39b8069bde2
Laxman <48151266+yetulaxman@users.noreply.github.com>
2020-05-18 00:30:23 +0300

    initial tests

--- slide_prep_puhti.sh
+++ slide_prep_puhti.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #SBATCH --time=00:15:00
 #SBATCH --partition=test
-#SBATCH --account=project_2001825
+#SBATCH --account=project_xxxx
 
+#clone current repo
 git clone https://github.com/yetulaxman/recipe-slideprep.git
 cd recipe-slideprep
+# download singularity image 
 wget https://a3s.fi/general/slidefactory.simg
 export TMPDIR=$PWD
+# convert .md file to corresponding .html file
 #singularity -s run -B $PWD:$PWD  slidefactory.simg  demo.md
 echo "creating html file from markdown  file" 
 singularity -s exec -B $PWD:$PWD  slidefactory.simg  python convert.py demo.md
